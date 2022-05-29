5b107bd09112a0b6dffb4616ec878c223ad7ec32
VishalRai1407 <100395867+VishalRai1407@users.noreply.github.com>
2022-05-29 14:28:51 +0530

    Delete Fundamentals_Of_Java directory

--- Fundamentals_Of_Java/Unit_01/P1_Task_01_FirstJavaProgram.java
+++
@@ -1,8 +0,0 @@
-package Unit_01;
-
-public class P1_Task_01_FirstJavaProgram {
-	public static void main(String [] args) {
-		System.out.println("Badri Vishal Rai");
-		System.out.println("First Java Program");
-	} 
-}
